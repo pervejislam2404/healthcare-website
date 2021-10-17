a1427d228db3ac3974d50de7cd44e10ed47a2287
pervejislam2404 <pervejislam2404@gmail.com>
2021-10-18 02:44:41 +0600

    update

--- src/components/Header.js
+++ src/components/Header.js
@@ -15,7 +15,7 @@ const Header = () => {
                     navbarScroll
                 >
                     <Nav.Link href="#action1">Home</Nav.Link>
-                    <Nav.Link href="#action2">Login</Nav.Link>
+                    <Nav.Link href="#action2">Login page</Nav.Link>
                     {/* <NavDropdown title="Link" id="navbarScrollingDropdown">
                     <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
                     <NavDropdown.Item href="#action4">Another action</NavDropdown.Item>
